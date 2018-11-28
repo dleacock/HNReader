fdaa0c65d8758f83df5472bd554c04e8e4a8b4da
David Leacock <dave.leacock@gmail.com>
2018-11-28 00:20:56 -0600

    Removed <ul> tag from Post. Shouldn't of been there in the first place. DWL

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,9 +4,7 @@ import PropTypes from 'prop-types';
 // Simple stateless component. It wraps a link to the post props from parent.
 const Post = (props) => {
     return (
-        <ul>
             <a href={props.url}>{props.title}</a>
-        </ul>
     );
 };
 
